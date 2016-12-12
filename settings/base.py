6bc0859932b069012167d05dce12310d10f347d2
Guinslym <guinslym@gmail.com>
2016-12-12 15:49:19 -0500

    django messages removed

--- settings/base.py
+++ settings/base.py
@@ -83,7 +83,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'django.template.context_processors.i18n',
-                'django_messages.context_processors.inbox',
+                #'django_messages.context_processors.inbox',
             ],
         },
     },
