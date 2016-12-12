d774bf4d3abeaaad8a5629259e4007d67169bf32
Guinslym <guinslym@gmail.com>
2016-12-12 15:19:42 -0500

    initial commit

---
+++ settings/dispatch.py
@@ -0,0 +1,13 @@
+import socket
+
+#My laptop is name 'Guinsly-thinkpad-lenovo'
+if 'MRT' in socket.gethostname():
+    from .development import *
+    print('--dev--settings--')
+else:
+    # if you cloned this app
+    # rename the file settings/production_py_example to
+    # production.py
+    from .production import *
+    #print('prod--settings')
+#this file won't be load in git and in the
