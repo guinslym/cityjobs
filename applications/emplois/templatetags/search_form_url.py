2487bd8b099db0d41f52d9bfc822d195410965ec
Guinslym <guinslym@gmail.com>
2017-03-22 14:06:28 -0400

    homepage menu

--- applications/emplois/templatetags/search_form_url.py
+++ applications/emplois/templatetags/search_form_url.py
@@ -6,8 +6,16 @@ register = template.Library()
 
 @register.simple_tag
 def seach_form_url(absolute_url="http://127.0.0.1:8000/en/", language_code='en'):
+    """return the searchForm url based on the language of the website"""
     #import ipdb;ipdb.set_trace()
     url = absolute_url.split('/') #['http:', '', '127.0.0.1:8000', 'en', '']
     url = url[0] +'//' +  url[2] +'/' + language_code + '/'
     return url+"searchJobs/"
 
+@register.simple_tag
+def get_site_url(absolute_url="http://127.0.0.1:8000/en/", language_code='en'):
+    """Check the language of the site and return the root url"""
+    #import ipdb;ipdb.set_trace()
+    url = absolute_url.split('/') #['http:', '', '127.0.0.1:8000', 'en', '']
+    url = url[0] +'//' +  url[2] +'/' + url[3]  + '/'
+    return url
